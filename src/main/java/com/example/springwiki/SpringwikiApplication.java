5d169d9b8be882ea60a023a2a8b2662d946c4fe6
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-05-15 18:31:42 +1000

    项目创建

---
+++ src/main/java/com/example/springwiki/SpringwikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springwiki/SpringwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
