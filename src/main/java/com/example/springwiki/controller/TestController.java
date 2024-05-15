63dcda4a4318e32f0da3590b08c2509c5a0b1495
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-05-15 21:23:32 +1000

    集成热部署

--- src/main/java/com/example/springwiki/controller/TestController.java
+++ src/main/java/com/example/springwiki/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.springwiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+
+    @Value("${hello.test:T}")
+    private  String test;
     @RequestMapping("/hello")
     public  String hello(){
-        return "Hello world";
+        return "Hello world"+test;
     }
+    @PostMapping("/hello/post")
+    public  String helloPost(String name){
+        return "Hello world"+name;
+    }
+
 }
