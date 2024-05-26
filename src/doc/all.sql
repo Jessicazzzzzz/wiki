230f805cf42ec8c37942a3c1a0a09aaba742e4e0
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-05-26 18:04:13 +1000

    新增了ebook 表，生成了持久层代码

--- src/doc/all.sql
+++ src/doc/all.sql
@@ -14,3 +14,25 @@ create table demo (
                       primary key (`id`)
 )engine=innodb default  charset = utf8mb4 comment ='测试';
 insert into  demo (id, name) values (1,'测试');
+
+drop table if exists ebook;
+create table ebook (
+    id bigint not null  comment 'id',
+    name varchar(50) comment '名称',
+    category1_id bigint comment '分类1',
+    category2_id bigint comment  '分类2',
+    description varchar(200) comment '描述',
+    cover varchar(200) comment '封面',
+    doc_count int comment '文档数',
+    view_count int comment  '阅读数',
+    vote_count int comment '点赞数',
+    primary key (`id`)
+
+)engine =innodb default charset =utf8mb4 comment '电子书';
+
+
+INSERT INTO ebook (id, name, category1_id, category2_id, description, cover, doc_count, view_count, vote_count) VALUES (1, 'Spring Boot 入门教程', 100, 101, '零基础入门 Java 开发，企业级应用开发最佳首选框架', null, 6, 22, 3);
+INSERT INTO ebook (id, name, category1_id, category2_id, description, cover, doc_count, view_count, vote_count) VALUES (2, 'Vue 入门教程', 100, 101, '零基础入门 Vue 开发，企业级应用开发最佳首选框架', null, 0, 0, 0);
+INSERT INTO ebook (id, name, category1_id, category2_id, description, cover, doc_count, view_count, vote_count) VALUES (3, 'Python 入门教程', 300, 301, '零基础入门 Python 开发，企业级应用开发最佳首选框架', null, 0, 0, 0);
+INSERT INTO ebook (id, name, category1_id, category2_id, description, cover, doc_count, view_count, vote_count) VALUES (4, 'Mysql 入门教程', 400, 401, '零基础入门 Mysql 开发，企业级应用开发最佳首选框架', null, 0, 0, 0);
+INSERT INTO ebook (id, name, category1_id, category2_id, description, cover, doc_count, view_count, vote_count) VALUES (5, 'Oracle 入门教程', 100, 101, '零基础入门 Oracle 开发，企业级应用开发最佳首选框架', null, 0, 0, 0);
