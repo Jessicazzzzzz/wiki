453909c40b5273b589278749e157dac1cbea8947
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-06-02 16:09:30 +1000

    增加了通用的返回类CommonResponse , 确定了返回值的格式

--- src/main/java/com/example/springwiki/controller/EbookController.java
+++ src/main/java/com/example/springwiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.example.springwiki.controller;
 
 import com.example.springwiki.domain.Ebook;
+import com.example.springwiki.resp.CommonResp;
 import com.example.springwiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +21,11 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return  ebookService.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> response = new CommonResp<>();
+        List<Ebook> list =   ebookService.list();
+        response.setContent(list);
+        return response;
     }
 
 
---
+++ src/main/java/com/example/springwiki/resp/CommonResp.java
@@ -0,0 +1,36 @@
+package com.example.springwiki.resp;
+
+/**
+ * @author jessica~
+ * @version 1.0
+ */
+public class CommonResp<T>{
+    private  boolean success = true;
+    private  String message;
+
+    private  T content;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+}
