11689add9d5979c5dccb2a349f651a741094c28b
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-06-23 19:51:15 +1000

    增加文档页面, 点击电子书可以进入文档页面

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -4,6 +4,7 @@ import AboutView from "@/views/AboutView.vue";
 import AdminEbook from "@/views/admin/Admin-ebook.vue";
 import AdminCategory from "@/views/admin/Admin-category.vue";
 import AdminDoc from "@/views/admin/Admin-doc.vue";
+import Doc from "@/views/DocView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -19,6 +20,11 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: "/doc",
+    name: "Doc",
+    component: Doc,
   },
   {
     path: "/admin/ebook",
