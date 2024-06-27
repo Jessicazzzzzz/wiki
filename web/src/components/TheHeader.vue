da3c709610619b4c4b691abb98db0372577c8154
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-06-27 21:55:55 +1000

    前端增加登录框

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -18,14 +18,76 @@
         <router-link to="/about"> 关于我们</router-link>
       </a-menu-item>
     </a-menu>
+    <a class="login-menu" @click="showLoginModal">
+      <span>登录</span>
+    </a>
+    <a-modal
+      title="登录"
+      v-model:visible="loginModalVisible"
+      :confirm-loading="loginModalLoading"
+      @ok="login"
+    >
+      <a-form
+        :model="loginUser"
+        :label-col="{ span: 6 }"
+        :wrapper-col="{ span: 18 }"
+      >
+        <a-form-item label="登录名">
+          <a-input v-model:value="loginUser.loginName"></a-input>
+        </a-form-item>
+        <a-form-item label="密码">
+          <a-input v-model:value="loginUser.password"></a-input>
+        </a-form-item>
+      </a-form>
+    </a-modal>
   </a-layout-header>
 </template>
 
-<style scoped></style>
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.login-menu {
+  color: #cccccc;
+}
+</style>
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "TheHeader",
+  setup() {
+    const loginUser = ref({
+      loginName: "test",
+      password: "test",
+    });
+
+    const loginModalVisible = ref(false);
+    const loginModalLoading = ref(false);
+    const showLoginModal = () => {
+      loginModalVisible.value = true;
+    };
+    // 退出登录
+    const login = () => {
+      console.log("开始登录");
+    };
+
+    return {
+      login,
+      loginUser,
+      loginModalVisible,
+      loginModalLoading,
+      showLoginModal,
+    };
+  },
 });
 </script>
