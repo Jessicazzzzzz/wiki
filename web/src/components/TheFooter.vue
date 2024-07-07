1a523b4ecea037a0cca2c5dcdb98ee226b994436
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-07-07 17:55:45 +1000

    使用vuex保存用户信息,方便其他组件获取登录信息,遗留问题,刷新后还是无法保存值

--- web/src/components/TheFooter.vue
+++ web/src/components/TheFooter.vue
@@ -1,12 +1,21 @@
 <template>
-  <a-layout-footer style="text-align: center"> Jess电子书 </a-layout-footer>
+  <a-layout-footer style="text-align: center">
+    Jess电子书 {{ user.name }}
+  </a-layout-footer>
 </template>
 
 <style scoped></style>
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
+import store from "@/store";
 
 export default defineComponent({
   name: "TheFooter",
+  setup() {
+    const user = computed(() => store.state.user);
+    return {
+      user,
+    };
+  },
 });
 </script>
