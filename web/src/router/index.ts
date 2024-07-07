a3e3b5ab0c022e418c28da4aa48581338db1f767
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-07-07 21:29:27 +1000

    增加路由登录拦截,未登录时跳到首页

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -6,6 +6,8 @@ import AdminEbook from "@/views/admin/Admin-ebook.vue";
 import AdminCategory from "@/views/admin/Admin-category.vue";
 import AdminDoc from "@/views/admin/Admin-doc.vue";
 import Doc from "@/views/DocView.vue";
+import store from "@/store";
+import { Tool } from "@/util/tool";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -31,27 +33,60 @@ const routes: Array<RouteRecordRaw> = [
     path: "/admin/user",
     name: "AdminUser",
     component: AdminUser,
+    meta: {
+      loginRequire: true,
+    },
   },
   {
     path: "/admin/ebook",
     name: "AdminEbook",
     component: AdminEbook,
+    meta: {
+      loginRequire: true,
+    },
   },
   {
     path: "/admin/doc",
     name: "AdminDoc",
     component: AdminDoc,
+    meta: {
+      loginRequire: true,
+    },
   },
   {
     path: "/admin/category",
     name: "AdminCategory",
     component: AdminCategory,
+    meta: {
+      loginRequire: true,
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (
+    to.matched.some(function (item) {
+      console.log(item, "是否需要登录校验：", item.meta.loginRequire);
+      return item.meta.loginRequire;
+    })
+  ) {
+    const loginUser = store.state.user;
+    if (Tool.isEmpty(loginUser)) {
+      console.log("用户未登录！");
+      next("/");
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
