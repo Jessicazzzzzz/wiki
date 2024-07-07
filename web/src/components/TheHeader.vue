cbcfb9894e7991690bf8d8c12fe88bcaf4b7ee79
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-07-07 21:20:37 +1000

    前端未登录,隐藏菜单

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -5,6 +5,13 @@
       <a-menu-item key="/">
         <router-link to="/">首页</router-link>
       </a-menu-item>
+    </a-menu>
+    <a-menu
+      theme="dark"
+      mode="horizontal"
+      :style="{ lineHeight: '64px' }"
+      v-if="user.id"
+    >
       <a-menu-item key="/admin/user">
         <router-link to="/admin/user"> 用户管理</router-link>
       </a-menu-item>
@@ -14,6 +21,8 @@
       <a-menu-item key="/admin/category">
         <router-link to="/admin/category"> 分类管理</router-link>
       </a-menu-item>
+    </a-menu>
+    <a-menu theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
       <a-menu-item key="/about">
         <router-link to="/about"> 关于我们</router-link>
       </a-menu-item>
