5bd7c44bace6fac06f4b14b970ec4f006754abfb
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-08-10 14:07:12 +1000

    定时任务示例

--- src/main/java/com/example/springwiki/config/SpringwikiApplication.java
+++ src/main/java/com/example/springwiki/config/SpringwikiApplication.java
@@ -7,19 +7,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @ComponentScan("com.example")
 @MapperScan("com.example.springwiki.mapper")
+@EnableScheduling
 public class SpringwikiApplication {
-    private  static  final Logger LOG =  LoggerFactory.getLogger(SpringwikiApplication.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SpringwikiApplication.class);
+
     public static void main(String[] args) {
 //        SpringApplication.run(SpringwikiApplication.class, args);
         SpringApplication app = new SpringApplication(SpringwikiApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功");
-        LOG.info("地址 http://localhost:{}",env.getProperty("server.port"));
+        LOG.info("地址 http://localhost:{}", env.getProperty("server.port"));
 
     }
 }
---
+++ src/main/java/com/example/springwiki/job/TestJob.java
@@ -0,0 +1,40 @@
+package com.example.springwiki.job;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author jessica~
+ * @version 1.0
+ */
+@Component
+public class TestJob {
+    private static final Logger LOG = LoggerFactory.getLogger(TestJob.class);
+
+    /**
+     * 固定时间间隔
+     */
+    @Scheduled(fixedRate = 1000)
+    public void simple() throws InterruptedException {
+        SimpleDateFormat sdf = new SimpleDateFormat("mm:ss");
+        String dateString = sdf.format(new Date());
+        Thread.sleep(2000);
+        LOG.info("每隔2秒执行一次:{}", dateString);
+    }
+
+    /**
+     * cron 跑批
+     */
+    @Scheduled(cron = "*/1 * * * * ?")
+    public void cron() throws InterruptedException {
+        SimpleDateFormat sdf = new SimpleDateFormat("mm:ss");
+        String dateString = sdf.format(new Date());
+        Thread.sleep(1500);
+        LOG.info("每隔1.5秒执行一次:{}", dateString);
+    }
+}
